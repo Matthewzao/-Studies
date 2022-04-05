e28cc60d94184328210f51bf07caf133c4982885
Matthzao <mateusmoraes100@gmail.com>
2022-04-05 17:31:11 -0300

    update

--- Matthew/AULAS PYTHON/Udemy/Lista(arrays).py
+++ Matthew/AULAS PYTHON/Udemy/Lista(arrays).py
@@ -1,2 +1,20 @@
-type (1)
-print(type)
+lista = list("Matthew")
+# print(lista)
+lista1 =[1,233,33,54,78,8,97]
+
+
+# PROCURANDO DADO EM UMA LISTA
+# num = 10
+# lista2 = list(range(11))
+# if 8 in lista2:
+#     print(f"achei o {num}")
+# else:
+#     print(f"não achei o {num}")
+
+# ORDENANDO LISTA
+# lista1.sort()
+# print(lista1)
+
+lista.sort()
+print(lista)
+
