941217ded337676d567c00822983cf35197ab812
Matthzao <mateusmoraes100@gmail.com>
2022-04-05 16:08:54 -0300

    added more codes

---
+++ Matthew/AULAS PYTHON/Guanabara/adivinhaçãoSimples.py
@@ -0,0 +1,13 @@
+from random import randint
+import time
+
+
+print("vou pensar em um numero, adivinhe!")
+pc = randint(0,1)
+ask = int(input("que numero eu pensei? "))
+if ask == pc:
+    print("você ganhou")
+else:
+    print("você perdeu, eu pensei no numero",pc,"e não no",ask)
+
+
--- Matthew/AULAS PYTHON/Udemy/Break.py
+++ Matthew/AULAS PYTHON/Udemy/Break.py
@@ -1,10 +1,10 @@
 # Exemplo 1
-# for num in range(1,11):
-#     if num == 6:
-#         break
-#     else:
-#         print(num)
-# print("fim")
+for num in range(1,11):
+    if num == 6:
+        break
+    else:
+        print(num)
+print("fim")
 
 # Exemplo 2
 while True:
