3d4992afd6d02040f7d4d8515e24a59d4baa6bed
Matthzao <mateusmoraes100@gmail.com>
2022-04-19 17:28:07 -0300

    tabuada

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	float a,b,c,delta,x1,x2
+	
+	printf("calculando raizes de uma funcao do 2 grau \n");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+};
